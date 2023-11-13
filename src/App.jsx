c0f7e699c987c667a9bd9753c08f739aa04b4d35
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-13 23:49:07 +0000

    Criação das tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
       <br />
