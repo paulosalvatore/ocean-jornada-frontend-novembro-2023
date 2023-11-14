8f7c958d00d938bd9de5771e79c54efba244f368
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-14 00:46:26 +0000

    Extração do conteúdo para o `<Card />`

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import Card from './components/Card/Card';
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
       <br />
+      <Card />
       <Card />
     </>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,11 @@
 export default function Card() {
-  return <div>Card está funcionando!</div>
+  return <div className="card">
+    <h2>Rick Sanchez</h2>
+    <div className="tags">
+      <div className="tag">Status: Vivo</div>
+      <div className="tag">Espécie: Humana</div>
+      <div className="tag">Origem: Terra C-137</div>
+    </div>
+    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+  </div>
 }
