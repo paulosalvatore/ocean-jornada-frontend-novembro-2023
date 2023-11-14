fcb9a095f427141b6e607c1160d031fa25009c55
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-14 22:53:29 +0000

    Correção do desafio 2: componente + props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
+import Tag from "../Tag/Tag"
+
 export default function Card(props) {
   const item = props.item
 
   return <div className="card">
     <h2>{item.name}</h2>
     <div className="tags">
-      <div className="tag">Status: Vivo</div>
-      <div className="tag">Espécie: Humana</div>
-      <div className="tag">Origem: Terra C-137</div>
+      <Tag text="Status: Vivo" />
+      <Tag text="Espécie: Humana" />
+      <Tag text="Origem: Terra C-137" />
     </div>
     <img src={item.imageUrl} />
   </div>
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,4 @@
+export default function Tag(props) {
+  const text = props.text;
+  return <div className="tag">{text}</div>
+}
