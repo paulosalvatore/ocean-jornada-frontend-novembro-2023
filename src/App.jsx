da5421ef302333624799a77634ed054766094414
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-14 23:06:38 +0000

    Correção do desafio 2: bônus array + map

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Card from './components/Card/Card';
 function App() {
   const item1 = {
     name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
 
   const item2 = {
     name: "Morty Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    tags: ["Origem: Terra C-137"]
   }
 
   const item3 = {
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,12 +3,14 @@ import Tag from "../Tag/Tag"
 export default function Card(props) {
   const item = props.item
 
+  const tags = item.tags || []
+
   return <div className="card">
     <h2>{item.name}</h2>
     <div className="tags">
-      <Tag text="Status: Vivo" />
-      <Tag text="Espécie: Humana" />
-      <Tag text="Origem: Terra C-137" />
+      {tags.map(function (element) {
+        return <Tag text={element} />
+      })}
     </div>
     <img src={item.imageUrl} />
   </div>
