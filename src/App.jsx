a27e2a9e86d8ad058425f4ad5c7ef42531fddbdd
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-15 01:14:27 +0000

    Criação da página `<ReadById />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import ReadById from './components/ReadById/ReadById';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<ReadById />} />
       </Routes>
     </BrowserRouter>
   )
