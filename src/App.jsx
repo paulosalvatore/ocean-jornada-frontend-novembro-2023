4755ebe421a3b654ed793b37ade499edf6269ff0
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-15 01:00:16 +0000

    Implementação das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,13 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import Card from './components/Card/Card';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
 
-function App() {
-  const [items, setItems] = useState([])
-
-  async function carregarDadosApi() {
-    const apiUrl = "https://rickandmortyapi.com/api/character/"
-
-    const response = await fetch(apiUrl)
-
-    const body = await response.json()
-
-    const results = body.results.map(function (element) {
-      return {
-        name: element.name,
-        image: element.image,
-        tags: [
-          `Status: ${element.status}`,
-          `Species: ${element.species}`,
-          `Origin: ${element.origin.name}`,
-          `Episodes: ${element.episode.length}`
-        ]
-      }
-    })
-
-    setItems(results)
-  }
-
-  useEffect(function () {
-    carregarDadosApi()
-  }, [])
-
+export default function App() {
   return (
-    <>
-      <div className="cards">
-        {items.map(function (element) {
-          return <Card item={element} />
-        })}
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App;
