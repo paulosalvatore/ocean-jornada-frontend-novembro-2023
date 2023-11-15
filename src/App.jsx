409a8bc7b8bd118861a1a4f3584dec1b0b5d6bdd
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-15 00:28:33 +0000

    Remoção de código antigo dos `items`

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,6 @@ import './App.css'
 import Card from './components/Card/Card';
 
 function App() {
-  const item1 = {
-    name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
-  }
-
-  const item2 = {
-    name: "Morty Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    tags: ["Origem: Terra C-137"]
-  }
-
-  const item3 = {
-    name: "Summer Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-  }
-
-  // const items = [item1, item2, item3]
-
-  // useState
   const [items, setItems] = useState([])
 
   async function carregarDadosApi() {
