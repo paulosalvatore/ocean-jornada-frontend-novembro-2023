917214b220c3facc07c9e7bf9e3340de6760270f
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-16 23:51:24 +0000

    Inserção do HTML da página

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -22,5 +22,39 @@ export default function ReadById() {
 
   // TODO: Exibir essas informações no JSX
 
-  return <div>ReadById: {id} - {item.name}</div>
+  // return <div>ReadById: {id} - {item.name}</div>
+
+  return (
+    <div className="main">
+      <div className="image-wrapper">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagem do Personagem" />
+      </div>
+
+      <div className="info">
+        <div className="info-tag">Status: Alive</div>
+
+        <div className="info-title">Rick Sanchez</div>
+
+        <div className="info-row">
+          <div className="info-label">Species:</div>
+          <div className="info-text">Human</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Gender:</div>
+          <div className="info-text">Male</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Origin:</div>
+          <div className="info-text">Earth (C-137)</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Location:</div>
+          <div className="info-text">Citadel of Ricks</div>
+        </div>
+      </div>
+    </div>
+  )
 }
