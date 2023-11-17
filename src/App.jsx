fa127228da876254fcdd96f45190b3d1eba42076
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-17 01:19:34 +0000

    Refatoração da rota para usar `createBrowserRouter`

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import ReadById from './components/ReadById/ReadById';
 
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/:id" element={<ReadById />} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<ReadById />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/:id",
+      element: <ReadById />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />
 }
