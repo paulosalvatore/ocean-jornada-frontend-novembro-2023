b804a4cf1f41422b937a5c31b7affc771e762cf6
Paulo Salvatore <salvatoregames@gmail.com>
2023-11-17 01:25:29 +0000

    Revert "Refatoração da rota para usar `createBrowserRouter`"
    This reverts commit fa127228da876254fcdd96f45190b3d1eba42076.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import ReadById from './components/ReadById/ReadById';
 
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/:id" element={<ReadById />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/:id",
-      element: <ReadById />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<ReadById />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
